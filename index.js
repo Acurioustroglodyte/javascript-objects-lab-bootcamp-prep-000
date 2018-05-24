04a3f25387be6c58039b4dab09d3a56d7e69a5b9
Trevor Jameson <tjameson15@gmail.com>
2018-05-24 02:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  return Object.assign({}, delete object[key])
+  return Object.assign({}, delete object[key]);
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
 }
